4a008a6e95881be3bb873c723bc8e07a50374e54
nitish24n <nitish24m@gmail.com>
2018-05-12 18:51:55 +0530

    annotation - Qualifier for dependency injection part 2nd of 2

--- com/luv2code/springdemo/RandomFortuneService.java
+++ com/luv2code/springdemo/RandomFortuneService.java
@@ -1,14 +1,29 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
-
+	
+	//create an array of strings
+	private String[] myFortunes = {
+		"Beware of the dogs.. they might bite you today.",
+		"Today will be the best day of your life.",
+		"You will get some new good friends today."
+	};
+	
+	//generate a random number generator
+	private Random myRandom = new Random();
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		//Pick a random string from array
+		int index = myRandom.nextInt(myFortunes.length);
+		String theFortune = myFortunes[index];
+		
+		return theFortune;
 	}
 
 }
