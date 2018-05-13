3fb96c1896956f95d552dc5b62179b19662a8659
nitish24n <nitish24m@gmail.com>
2018-05-13 23:50:11 +0530

    injecting values from properties file

--- com/luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -11,12 +11,16 @@ public class SwimJavaConfigDemoApp {
 				new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		//get bean from spring container
-		Coach theCoach = context.getBean("swimCoach",Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach",SwimCoach.class);
 		
 		//call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		System.out.println(theCoach.getDailyFortune());
+		
+		System.out.println("Team : "+theCoach.getTeam());
+		
+		System.out.println("Email : "+theCoach.getEmail());
 		
 		//close the context
 		context.close();
